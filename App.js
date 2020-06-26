59908f637a664cdf222abe03e03e0fc71945fbd8
akshaypatil581 <akshaypatil581@gmail.com>
2020-06-26 05:43:28 -0700

    on Press

--- App.js
+++ App.js
@@ -2,11 +2,11 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
-  console.log("Hello");
+  const handlePress = () => console.log("Hi Akshay Hello");
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1}>
+      <Text numberOfLines={1} onPress={handlePress}>
         Hello Akshay! Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Odit hic ex quod obcaecati cupiditate, autem asperiores commodi ab eaque
         in minus magnam fugiat amet illum voluptatum perspiciatis ipsum.
